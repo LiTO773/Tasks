001565013086a6c2a6611e0d5e652a0d91916966
Lucky773 <luispinto2000@gmail.com>
2016-10-31 17:22:57 +0000

    URLs definidos TODO: Adicionar funçõs respetivas, Conseguir servir ficheiros estáticos, Base de Dados

--- main/main.go
+++ main/main.go
@@ -6,13 +6,38 @@ import (
 )
 
 func main() {
-	// URLs
-	http.HandleFunc("/", CompleteTaskFunc)
+	// Relativo a tarefas específicas
+	http.HandleFunc("/terminar/", TerminarTarefa)
+	http.HandleFunc("/eliminar/", EliminarTarefa)
+	http.HandleFunc("/editar/", EditarTarefa)
+	http.HandleFunc("/restaurar/", RestaurarTarefa)
+	http.HandleFunc("/criar/", CriarTarefa)
+	http.HandleFunc("/atualizar/", AtualizarTarefa)
+	http.HandleFunc("/procurar/", ProcurarTarefa)
 
-	log.Fatal(http.ListenAndServe(":8080", nil)) // O servidor está ativo em: localhost:8080
+	// Relativo a todas as tarefas
+	http.HandleFunc("/", MostrarTarefas)
+	http.HandleFunc("/eliminado/", TarefasEliminadas)
+	http.HandleFunc("/reciclagem/", TarefasRecicladas)
+	http.HandleFunc("/terminado/", TarefasTerminadas)
+
+	// Relativo ao utilizador
+	http.HandleFunc("/entrar", Login)
+	http.HandleFunc("/registar", Signin)
+	http.HandleFunc("/admin", Administracao)
+	http.HandleFunc("/adicionar_utilizador", AdicionarUtilizador)
+	http.HandleFunc("/alterar", AlterarUtilizador)
+	http.HandleFunc("/sair", Logout)
+
+	// Ficheiros estáticos
+	http.Handle("/static/", http.FileServer(http.Dir("../public")))
+
+	log.Print("O servidor está ativo na porta 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func CompleteTaskFunc(w http.ResponseWriter, r *http.Request) { // "/"
-	// Escreve para o corpo do documento
-	w.Write([]byte("<h1>Bem-vindo ao meu site!</h1>"))
+// MostrarTarefas Mostrar todas tarefas pendentes do utilizador ("/")
+func MostrarTarefas(w http.ResponseWriter, r *http.Request) {
+	message := "Todas as tarefas pendentes (" + r.Method + ")" //r.Method == "GET" || r.Method == "POST"
+	w.Write([]byte(message))
 }
