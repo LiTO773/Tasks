e38ddcb9d9125a2605e35f71e72a469dff728571
Lucky773 <luispinto2000@gmail.com>
2016-10-31 14:35:59 +0000

    URL "/" adicionado

--- main.go
+++ main.go
@@ -8,5 +8,14 @@ import (
 func main() {
 	PORT := "127.0.0.1:8080" // localhost:8080
 	log.Print("O servidor está ativo em: " + PORT)
+
+	// URLs
+	http.HandleFunc("/", CompleteTaskFunc)
+
 	log.Fatal(http.ListenAndServe(PORT, nil))
+}
+
+func CompleteTaskFunc(w http.ResponseWriter, r *http.Request) { // "/"
+	// Escreve para o corpo do documento
+	w.Write([]byte("<h1>Bem-vindo ao meu site!</h1>"))
 }
